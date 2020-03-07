c4327eaee1781feac576a13a96128fb0d9dc121e
asaf.arviv <asaf.arviv@gmail.com>
2020-03-07 21:03:18 +0200

    first commit

--- src/main/java/com/asaf/auth/WebApplication.java
+++ src/main/java/com/asaf/auth/WebApplication.java
@@ -2,7 +2,7 @@ package com.asaf.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//yhfghfjhfjhgf
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) throws Exception {
